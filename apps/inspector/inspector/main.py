02951ad95b4aed44b7d6a4c15282594329f0bbe6
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-11-15 23:26:13 +0000

    list accounts

--- apps/inspector/inspector/main.py
+++ apps/inspector/inspector/main.py
@@ -1,2 +1,39 @@
+import boto3
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+
+
+def assume_role(role_arn):
+    sts_client = boto3.client("sts")
+    assumed_role_object = sts_client.assume_role(
+        RoleArn=role_arn,
+        RoleSessionName="horatio-inspector"
+    )
+    return assumed_role_object["Credentials"]
+
 def handler(event, _context):
     print(f"handling event: {event}")
+
+    list_accounts_credentials = assume_role("arn:aws:iam::008356366354:role/horatio-list-accounts-role")
+
+    organisations_client = boto3.client(
+        "organizations",
+        aws_access_key_id=list_accounts_credentials["AccessKeyId"],
+        aws_secret_access_key=list_accounts_credentials["SecretAccessKey"],
+        aws_session_token=list_accounts_credentials["SessionToken"]
+    )
+    organisation_accounts_paginator = organisations_client.get_paginator("list_accounts")
+
+    organisation_accounts_results = [
+        result["Accounts"]
+        for result in organisation_accounts_paginator.paginate()
+    ]
+
+    organisation_accounts = flatten(organisation_accounts_results)
+
+    for account in organisation_accounts:
+        account_id = str(account["Id"])
+        account_name = account["Name"]
+
+        print(f"processing account {account_name} ({account_id})")
