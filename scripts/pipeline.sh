9b09ecdf4960f5fc70f4d50987c1bc306670b4ff
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-11-15 13:37:00 +0000

    have a single pipeline

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -2,7 +2,7 @@ set -e
 
 aws cloudformation deploy \
   --template-file ci/codepipeline.yml \
-  --stack-name codepipeline-horatio-common-resources \
+  --stack-name codepipeline-horatio \
   --capabilities CAPABILITY_IAM \
   --region eu-west-1 \
   --profile accounts-janitor
