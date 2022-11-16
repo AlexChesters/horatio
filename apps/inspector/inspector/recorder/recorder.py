86f11fe7cc4928731e6d88fe90e68a2d13c86da3
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-11-16 20:37:48 +0000

    post to queue

--- apps/inspector/inspector/recorder/recorder.py
+++ apps/inspector/inspector/recorder/recorder.py
@@ -1,22 +1,22 @@
 import os
 import datetime
+import json
 
 import boto3
 
-dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table(os.environ["TABLE_NAME"])
+sqs = boto3.resource("sqs")
+queue = sqs.Queue(os.environ["QUEUE_URL"])
 
 def record(account_id, rule_name, result):
     print(f"recording result {result} for account {account_id}")
 
     today = datetime.datetime.today()
-    thirty_days_from_today = today + datetime.timedelta(30)
 
-    table.put_item(
-        Item={
-            "partition_key": f"{account_id}|{rule_name}",
+    queue.send_message(
+        MessageBody=json.dumps({
+            "account_id": account_id,
+            "rule_name": rule_name,
             "inspection_date": f"{today.year}-{today.month}-{today.day}",
-            "result": result,
-            "ttl": int(thirty_days_from_today.timestamp())
-        }
+            "result": result
+        })
     )
