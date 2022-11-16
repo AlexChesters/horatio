29af4dea918f0aaad8989b3fcc01eca9b7665d61
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-11-16 20:38:15 +0000

    remove invoke lambda script

--- scripts/invoke-lambda.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-OUTPUT=$(aws lambda invoke \
-  --function-name $1 \
-  --log-type Tail \
-  --profile accounts-janitor \
-  lambda_invoke_output.txt)
-
-echo $OUTPUT | jq -r .LogResult | base64 --decode | tee lambda_invoke_log.txt
