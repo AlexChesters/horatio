19818b9fd57fdceb1fa53d7b6e29bfb757c39dca
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-11-16 19:56:39 +0000

    add ttls

--- apps/inspector/inspector/recorder/recorder.py
+++ apps/inspector/inspector/recorder/recorder.py
@@ -10,11 +10,13 @@ def record(account_id, rule_name, result):
     print(f"recording result {result} for account {account_id}")
 
     today = datetime.datetime.today()
+    thirty_days_from_today = today + datetime.timedelta(30)
 
     table.put_item(
         Item={
             "partition_key": f"{account_id}|{rule_name}",
             "inspection_date": f"{today.year}-{today.month}-{today.day}",
-            "result": result
+            "result": result,
+            "ttl": int(thirty_days_from_today.timestamp())
         }
     )
