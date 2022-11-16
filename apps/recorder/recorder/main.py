2b2c0966c6a7f911d433b5a1149f62c679ac4ed4
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-11-16 21:27:43 +0000

    write to table

--- apps/recorder/recorder/main.py
+++ apps/recorder/recorder/main.py
@@ -1,5 +1,34 @@
+import datetime
+import os
+import json
+
+import boto3
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(os.environ["TABLE_NAME"])
+
 def handler(event, _context):
     print(f"handling event: {event}")
+
+    today = datetime.datetime.today()
+    thirty_days_from_today = today + datetime.timedelta(30)
+
+    for record in event["Records"]:
+        message_body = json.loads(record["body"])
+
+        account_id = message_body["account_id"]
+        rule_name = message_body["rule_name"]
+        inspection_date = message_body["inspection_date"]
+        report = message_body["report"]
+
+        table.put_item(
+            Item={
+                "partition_key": f"{account_id}|{rule_name}",
+                "inspection_date": inspection_date,
+                "report": report,
+                "ttl": int(thirty_days_from_today.timestamp())
+            }
+        )
 
 if __name__ == "__main__":
     handler({}, None)
