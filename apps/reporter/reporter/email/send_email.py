95e84256a7eae6e12180676ced3b7d6ff250124e
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-11-19 23:02:21 +0000

    add date to subject

--- apps/reporter/reporter/email/send_email.py
+++ apps/reporter/reporter/email/send_email.py
@@ -1,7 +1,11 @@
+import datetime
+
 import boto3
 
 def send_email(body):
     client = boto3.client("ses", region_name="eu-west-1")
+
+    today = datetime.datetime.today()
 
     client.send_email(
         Source="horatio@alexchesters.com",
@@ -11,7 +15,7 @@ def send_email(body):
         Message={
             "Subject": {
                 "Charset": "UTF-8",
-                "Data": "Horatio Daily Report"
+                "Data": f"Horatio Report - {today.day}/{today.month}/{today.year}"
             },
             "Body": {
                 "Html": {
