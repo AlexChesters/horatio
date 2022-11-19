f7849722921da1cd4063ec619706b9c97622c341
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-11-19 19:48:33 +0000

    add query

--- apps/reporter/reporter/main.py
+++ apps/reporter/reporter/main.py
@@ -1,5 +1,27 @@
+import os
+import datetime
+
+import boto3
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(os.environ["TABLE_NAME"])
+
 def handler(event, _context):
     print(f"handling event: {event}")
+
+    today = datetime.datetime.today()
+
+    response = table.query(
+        IndexName="InspectionDateIndex",
+        KeyConditionExpression="inspection_date = :today",
+        ExpressionAttributeValues={
+            ":today": {
+                "S": f"{today.year}-{today.month}-{today.day}"
+            }
+        }
+    )
+
+    print(f"response: {response}")
 
 if __name__ == "__main__":
     handler({}, None)
