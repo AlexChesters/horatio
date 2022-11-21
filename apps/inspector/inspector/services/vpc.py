82de86826c5ce468d150a8d465e6b1c083a5e66f
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-11-21 00:24:00 +0000

    undo testing change

--- apps/inspector/inspector/services/vpc.py
+++ apps/inspector/inspector/services/vpc.py
@@ -23,24 +23,15 @@ def inspect(credentials, region):
     ])
 
     for vpc in vpc_results:
-        results.append({
-            "rule_name": "default_vpc_exists",
-            "report": {
-                "message": "Default VPC exists in account",
-                "remedy": "Delete the default VPC, replace with a custom one if a VPC is needed.",
-                "resource_id": vpc["VpcId"],
-                "region": region
-            }
-        })
-        # if vpc["IsDefault"]:
-        #     results.append({
-        #         "rule_name": "default_vpc_exists",
-        #         "report": {
-        #             "message": "Default VPC exists in account",
-        #             "remedy": "Delete the default VPC, replace with a custom one if a VPC is needed.",
-        #             "resource_id": vpc["VpcId"],
-        #             "region": region
-        #         }
-        #     })
+        if vpc["IsDefault"]:
+            results.append({
+                "rule_name": "default_vpc_exists",
+                "report": {
+                    "message": "Default VPC exists in account",
+                    "remedy": "Delete the default VPC, replace with a custom one if a VPC is needed.",
+                    "resource_id": vpc["VpcId"],
+                    "region": region
+                }
+            })
 
     return results
