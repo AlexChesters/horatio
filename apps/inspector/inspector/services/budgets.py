2f61709265c96ae8554ba7f7c1f888f4a713951f
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-11-23 15:34:04 +0000

    change call

--- apps/inspector/inspector/services/budgets.py
+++ apps/inspector/inspector/services/budgets.py
@@ -5,41 +5,40 @@ from inspector.utils.flatten import flatten
 def verify_budgets_exist(client, region, account_id):
     results = []
 
-    paginator = client.get_paginator("describe_budget_actions_for_account")
-    print(f"analysing actions for account {account_id}")
+    paginator = client.get_paginator("describe_budget_notifications_for_account")
+    print(f"analysing notifications for account {account_id}")
 
-    actions = flatten([
-        result.get("Actions", [])
+    notifications_for_account = flatten([
+        result.get("BudgetNotificationsForAccount", [])
         for result in paginator.paginate(AccountId=account_id)
     ])
 
-    account_has_action_for_forecast = False
-    account_has_action_for_actual = False
+    account_has_notification_for_forecast = False
+    account_has_notification_for_actual = False
 
-    print(f"analysing actions: {actions}")
-    for action in actions:
-        if action["NotificationType"] == "ACTUAL":
-            account_has_action_for_actual = True
-        elif action["NotificationType"] == "FORECASTED":
-            account_has_action_for_forecast = True
+    for notification in notifications_for_account.get("Notifications", []):
+        if notification["NotificationType"] == "ACTUAL":
+            account_has_notification_for_actual = True
+        elif notification["NotificationType"] == "FORECASTED":
+            account_has_notification_for_forecast = True
 
-    if not account_has_action_for_forecast:
+    if not account_has_notification_for_forecast:
         results.append({
-            "rule_name": "forecast_budget_with_action",
+            "rule_name": "forecast_budget_with_notification",
             "report": {
-                "message": "No forecast budget with an action exists",
-                "remedy": "Create a forecast budget with an associated action",
+                "message": "No forecast budget with an notification exists",
+                "remedy": "Create a forecast budget with an associated notification",
                 "resource_id": account_id,
                 "region": region
             }
         })
 
-    if not account_has_action_for_actual:
+    if not account_has_notification_for_actual:
         results.append({
-            "rule_name": "actual_budget_with_action",
+            "rule_name": "actual_budget_with_notification",
             "report": {
-                "message": "No actual budget with an action exists",
-                "remedy": "Create a actual budget with an associated action",
+                "message": "No actual budget with an notification exists",
+                "remedy": "Create a actual budget with an associated notification",
                 "resource_id": account_id,
                 "region": region
             }
