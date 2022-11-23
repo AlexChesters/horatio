4f199c5f755c1e475728205c839beb85c8773856
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-11-23 11:29:44 +0000

    pass account id

--- apps/inspector/inspector/services/budgets.py
+++ apps/inspector/inspector/services/budgets.py
@@ -2,14 +2,14 @@ import boto3
 
 from inspector.utils.flatten import flatten
 
-def verify_budgets_exist(client, _region):
+def verify_budgets_exist(client, _region, account_id):
     results = []
 
     paginator = client.get_paginator("describe_budgets")
 
     budgets = flatten([
         result["Budgets"]
-        for result in paginator.paginate()
+        for result in paginator.paginate(AccountId=account_id)
     ])
 
     for budget in budgets:
@@ -30,6 +30,14 @@ def inspect(credentials, region):
         aws_session_token=credentials["SessionToken"]
     )
 
-    results.extend(verify_budgets_exist(client, region))
+    account_id = boto3.client(
+        "sts",
+        region_name=region,
+        aws_access_key_id=credentials["AccessKeyId"],
+        aws_secret_access_key=credentials["SecretAccessKey"],
+        aws_session_token=credentials["SessionToken"]
+    ).get_caller_identity().get("Account")
+
+    results.extend(verify_budgets_exist(client, region, account_id))
 
     return results
