39d1f753af7220a341c7557aed05840182ac04cf
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2023-05-17 17:06:46 +0100

    add test

--- apps/inspector/tests/services/test_iam.py
+++ apps/inspector/tests/services/test_iam.py
@@ -2,6 +2,7 @@ import unittest
 
 from moto import mock_iam
 import boto3
+from freezegun import freeze_time
 
 from inspector.services.iam import inspect
 
@@ -29,7 +30,7 @@ class IAMTestsTests(unittest.TestCase):
         results = inspect(self.dummy_credentials, "eu-west-1")
         self.assertListEqual(results, [], "An IAM user without an access key should mean no results are produced")
 
-    def test_iam_user_with_access_key_with_recent_access_key_returns_no_results(self):
+    def test_iam_user_with_recent_access_key_returns_no_results(self):
         iam = boto3.resource("iam")
         user = iam.User("some-user")
         user.create()
@@ -37,3 +38,29 @@ class IAMTestsTests(unittest.TestCase):
 
         results = inspect(self.dummy_credentials, "eu-west-1")
         self.assertListEqual(results, [], "An IAM user with a recent access key should mean no results are produced")
+
+    @freeze_time("2023-05-17", as_kwarg="ft")
+    def test_iam_user_with_old_access_key_returns_a_result(self, ft):
+        iam = boto3.resource("iam")
+        user = iam.User("some-user")
+        user.create()
+        user.create_access_key_pair()
+
+        ft.move_to("2024-05-17")
+
+        results = inspect(self.dummy_credentials, "eu-west-1")
+        self.assertListEqual(
+            results,
+            [
+                {
+                    "rule_name": "iam_user_access_key_age",
+                    "report": {
+                        "message": "IAM user has an access key older than 30 days (366)",
+                        "remedy": "Rotate the access key.",
+                        "resource_id": "some-user",
+                        "region": "eu-west-1"
+                    }
+                }
+            ],
+            "An IAM user with an old access key should mean a result is produced"
+        )
