7f86a1554e5b8faed45fb0bade5c4adb92e5867f
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2023-05-17 16:56:22 +0100

    add no iam users test

--- apps/inspector/tests/services/test_iam.py
+++ apps/inspector/tests/services/test_iam.py
@@ -1,11 +1,21 @@
 import unittest
 
+from moto import mock_iam
+
+from inspector.services.iam import inspect
+
+@mock_iam
 class IAMTestsTests(unittest.TestCase):
     def setUp(self):
-        pass
+        self.dummy_credentials = {
+            "AccessKeyId": "testing",
+            "SecretAccessKey": "testing",
+            "SessionToken": "testing"
+        }
 
     def tearDown(self):
         pass
 
-    def test_example(self):
-        pass
+    def test_no_iam_users_results_in_no_results(self):
+        results = inspect(self.dummy_credentials, "eu-west-1")
+        self.assertListEqual(results, [], "No IAM users should mean no results are produced")
