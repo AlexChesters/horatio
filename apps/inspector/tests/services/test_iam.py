59428feec60e144eecd7b173435ee323d205b70a
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2023-05-17 16:58:59 +0100

    add test

--- apps/inspector/tests/services/test_iam.py
+++ apps/inspector/tests/services/test_iam.py
@@ -1,6 +1,7 @@
 import unittest
 
 from moto import mock_iam
+import boto3
 
 from inspector.services.iam import inspect
 
@@ -16,6 +17,14 @@ class IAMTestsTests(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_no_iam_users_results_in_no_results(self):
+    def test_no_iam_users_returns_no_results(self):
         results = inspect(self.dummy_credentials, "eu-west-1")
         self.assertListEqual(results, [], "No IAM users should mean no results are produced")
+
+    def test_iam_user_without_access_key_returns_no_results(self):
+        iam = boto3.resource("iam")
+        user = iam.User("some-user")
+        user.create()
+
+        results = inspect(self.dummy_credentials, "eu-west-1")
+        self.assertListEqual(results, [], "An IAM user without an access key should mean no results are produced")
