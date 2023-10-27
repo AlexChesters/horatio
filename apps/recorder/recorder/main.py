74c53c4104253b59c65d9f760d7face4b70bd1b4
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2023-10-27 15:13:37 +0100

    add recorder metrics

--- apps/recorder/recorder/main.py
+++ apps/recorder/recorder/main.py
@@ -3,11 +3,12 @@ import os
 import json
 
 import boto3
-from aws_lambda_powertools import Logger
-from aws_lambda_powertools import Tracer
+from aws_lambda_powertools import Logger, Tracer, Metrics, single_metric
+from aws_lambda_powertools.metrics import MetricUnit
 
 logger = Logger()
 tracer = Tracer()
+metrics = Metrics()
 
 @tracer.capture_lambda_handler
 @logger.inject_lambda_context(log_event=True)
@@ -35,6 +36,10 @@ def handler(event, _context):
                 "ttl": int(one_year_from_today.timestamp())
             }
         )
+
+        with single_metric(name="RecordedMessage", unit=MetricUnit.Count, value=1) as metric:
+            metric.add_dimension(name="RuleName", value=rule_name)
+            metric.add_dimension(name="AccountID", value=account_id)
 
 if __name__ == "__main__":
     handler({}, None)
